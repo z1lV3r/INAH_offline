5fbf8dfccda55c2a7ddf7c230767ea30b1308b8e
z1lV3r <zilver.thd@gmail.com>
2018-08-26 23:57:54 -0500

    detalle

---
+++ DetalleWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace INAH
+{
+    /// <summary>
+    /// Lógica de interacción para Window1.xaml
+    /// </summary>
+    public partial class DetalleWindow : Window
+    {
+        public DetalleWindow()
+        {
+            InitializeComponent();
+            this.Width = SystemParameters.PrimaryScreenWidth;
+            this.Height = SystemParameters.PrimaryScreenHeight;
+            this.MinWidth = SystemParameters.PrimaryScreenWidth;
+            this.MinHeight = SystemParameters.PrimaryScreenHeight;
+        }
+    }
+}
